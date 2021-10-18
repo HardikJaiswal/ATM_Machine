31e13e931d5f787b0bbe17797c535a87939c03ce
Hardik Jaiswal <56971067+HardikJaiswal@users.noreply.github.com>
2021-10-18 19:40:23 +0530

    Update Bank.cs

--- BankApplication.Models/Bank.cs
+++ BankApplication.Models/Bank.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace BankApplication.Models
 {
@@ -7,10 +7,12 @@ namespace BankApplication.Models
         public Dictionary<int, Account> AllAccounts = new Dictionary<int, Account>();
         public string BankName { get; set; }
         public List<string> TransactionList=new List<string>();
+        public string BankID;
 
         public Bank(string Name)
         {
             BankName = Name;
+            BankID = $"{Name.Substring(0, 3)}{System.DateTime.Today}";
             //TransactionList.Add("Date && Time \t\t Account Number \t\t Transaction \t\t Amount");
         }
     }
